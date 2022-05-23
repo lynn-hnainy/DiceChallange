ebe032e9e15503715a124ad0efd67de7956aa433
LH <lh@LHs-MacBook-Air.local>
2022-05-23 10:14:55 +0300

    JavaScript:Adding images to each player

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ window.onload = function afterWebPageLoad() {
     var number2=Math.floor(Math.random()*6)+1;
     //console.log(number1);
     //console.log(number2); 
-    img_src1=img_src+number1;
-    img_src2=img_src+number2;
+    img_src1=img_src+number1+".png";
+    img_src2=img_src+number2+".png";
     console.log(img_src1);
     console.log(img_src2);
+    var img1=document.getElementsByClassName("img1");
+    var img2=document.getElementsByClassName("img2");
+    img1[0].src=img_src1;
+    img2[0].src=img_src2;
 }
