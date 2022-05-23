d2e517c793bd772298298887183c8928c29d1674
LH <lh@LHs-MacBook-Air.local>
2022-05-23 10:07:29 +0300

    JavaScript:Generating two number from 1 to 6

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = function afterWebPageLoad() { 
+    var number1=Math.floor(Math.random()*6)+1;
+    var number2=Math.floor(Math.random()*6)+1;
+    console.log(number1);
+    console.log(number2); 
+}
